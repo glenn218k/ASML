2e7c878bb0f165f2a1165801e1bd2e43a49bb35c
professor murder <nicksucks44@gmail.com>
2013-03-19 14:37:16 -0700

    ok trying to reup

---
+++ Asml-Level100Pikachus/Asml-Level100Pikachus/Asml-Level100Pikachus/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace Asml_Level100Pikachus
+{
+    public abstract class FileReader
+    {
+        public FileReader() { }
+        /// <summary>
+        /// abstract ReadLines method
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public abstract List<Target> ReadLines(string filePath);
+    }
+}
