79b1ff9e91ee437c0cb1635ba66f8ed329267c43
professor murder <nicksucks44@gmail.com>
2013-04-11 16:11:26 -0700

    updated
    should be final update for this turn in

---
+++ Asml-Level100Pikachus/Asml-Level100Pikachus/Asml-Level100Pikachus/Factories/Factory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Asml_Level100Pikachus;
+
+namespace Asml_Level100Pikachus.Factories
+{
+    public class Factory
+    {
+        static Factory m_factory;
+
+        public static Factory GetInstance()
+        {
+            if (m_factory == null)
+            {
+                m_factory = new Factory();
+            }
+
+            return m_factory;
+        }
+
+        private Factory()
+        {
+        }
+
+        /// <summary>
+        /// creates a filereader depending on the filepath
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public FileReader CreateReader(string filePath)
+        {
+            FileReader reader = null;
+
+            string ext = Path.GetExtension(filePath);
+            ext = ext.ToLower();
+
+            // if .ini make an ini, if .xml make an xml
+            if (ext == ".ini")
+            {
+                reader = new INIReader();
+            }
+            else if (ext == ".xml")
+            {
+                reader = new XMLReader();
+            }
+            else
+            {
+
+            }
+
+            return reader;
+        }
+    }
+}
