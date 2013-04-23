72a3f6d657de49b912cebbc8c29ad07dea551621
Christopher Knight <christopherb.knight@email.wsu.edu>
2013-04-23 15:56:05 -0700

    commiting proj2 to master

---
+++ cpts323-spring-2013-project2-level100pikachus/Asml-Level100Pikachus/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Asml_Level100Pikachus
+{
+    class Camera
+    {
+        static Camera m_camera;
+        public Capture cap;
+
+        public static Camera GetInstance()
+        {
+            if (m_camera == null)
+            {
+                m_camera = new Camera();
+            }
+
+            return m_camera;
+        }
+
+        private Camera()
+        {
+            cap = new Capture();
+        }
+    }
+}
