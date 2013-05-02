7fe46701cb7c26ef4187ed380c87cb8219901d3d
Christopher Knight <christopherb.knight@email.wsu.edu>
2013-05-02 16:57:49 -0700

    addingproject3

---
+++ cpts323-2013-spring-project-03-level100pikachus/Asml-Level100Pikachus-project-03/Asml-Level100Pikachus/FriendFoeSD.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asml_Level100Pikachus
+{
+    class FriendFoeSD : SD
+    {
+        public int SearchandDestroy(bool reset)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.kachu);
+            System.Media.SoundPlayer playerInf = new System.Media.SoundPlayer(Properties.Resources.pikapika);
+
+            TargetManager tm = TargetManager.GetInstance();
+            Controller controller = Controller.GetInstance();
+
+            int i = 0;
+            foreach (Target target in tm.TMTargets)
+            {
+                i++;
+                
+                controller.MoveTo(target.x, target.y);
+
+                player.Play();
+                controller.Fire();
+                controller.SetNum(controller.GetNum() - 1);
+
+                playerInf.PlayLooping();
+
+                if (i != tm.TMTargets.Count)
+                {
+                    if (reset == true)
+                    {
+                        controller.Launcher.Reset();
+                    }
+                }
+
+                Thread.Sleep(250);
+            }
+
+            return i;
+        }
+    }
+}
---
+++ cpts323-2013-spring-project-03-level100pikachus/Asml-Level100Pikachus-project-03/Asml-Level100Pikachus/SD.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asml_Level100Pikachus
+{
+    class SD : Asml
+    {
+        public void SearchandDestroy(bool reset, bool friend, bool foe, bool friendfoe, bool live)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.kachu);
+            System.Media.SoundPlayer playerSound = new System.Media.SoundPlayer(Properties.Resources.pkmtheme);
+
+            TargetManager tm = TargetManager.GetInstance();
+
+            Controller controller = Controller.GetInstance();
+            controller.Launcher = new MissileLauncherAdapter();
+            Controller cont = Controller.GetInstance();
+
+            if(live == false)
+                controller.Launcher.Reset();
+
+            int i = 0;
+
+            if (friend == true)
+            {
+                FriendSD friendClass = new FriendSD();
+
+                i = friendClass.SearchandDestroy(reset);
+            }
+            else if (foe == true)
+            {
+                FoeSD foeClass = new FoeSD();
+
+                i = foeClass.SearchandDestroy(reset);
+            }
+            else
+            {
+                FriendFoeSD friendFoeClass = new FriendFoeSD();
+
+                i = friendFoeClass.SearchandDestroy(reset);
+            }
+
+            if (controller.GetNum() > 0)
+            {
+                playerSound.Play();
+                controller.MoveBy(6000, 0);
+                for (int j = 0; j < controller.GetNum();)
+                {
+                    controller.Fire();
+                    controller.SetNum(controller.GetNum() - 1);
+                }
+
+                controller.SetNum(0);
+                playerSound.Stop();
+            }       
+        }
+    }
+}
